f0efa80a44c554d04bd085ec72dec64f8657389e
qrav <szynkamaster@gmail.com>
2022-02-05 15:16:02 +0100

    ready for build on local

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import Head from "next/head";
+import React from "react";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
