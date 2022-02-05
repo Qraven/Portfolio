cff08f142ce51f04ab3025b09a1df415c27acd11
qrav <szynkamaster@gmail.com>
2022-02-05 23:57:06 +0100

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import React from "react";
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
