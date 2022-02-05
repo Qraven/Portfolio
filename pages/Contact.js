4bf78222d786094508d5bab8bb9f07fd7f56da06
qrav <szynkamaster@gmail.com>
2022-02-05 20:25:27 +0100

    added content to contact.js

--- pages/Contact.js
+++ pages/Contact.js
@@ -71,9 +71,30 @@ export default function contact()
 
             <div className="border-l-4 h-3/4 border-solid border-pink border-opacity-30 float-left absolute left-1/2 bottom-32 rounded-lg"></div>
 
-            <section className="w-1/4 float-right absolute bottom-16 right-52">
+            <section className="float-right absolute bottom-96 right-28 text-pink font-roboto font-light text-4xl">
+            <span>
+              <div className="">
+                <p className="float-left inline-block align-middle">
+                  <svg xmlns="http://www.w3.org/2000/svg" className=" h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                  </svg>
+                </p>
+                <a className=" my-12 relative top-2">szymonroman.kontakt@gmail.com</a>
+              </div>
+
+
+              <br></br>
               
-            </section>
+              <div className="">
+                <p className="float-left inline-block align-middle">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
+                  </svg>
+                </p>
+                <Link href="https://github.com/Qraven"><a target="_blank" className=" my-12 relative top-2">GitHub</a></Link>
+              </div>
+            </span>          
+          </section>
           </span>
 
           <footer className="absolute bottom-2 w-full h-4/12 text-sm text-footer text-opacity-50 bg-darkPurple shadow-xl">
